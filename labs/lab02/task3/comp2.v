9c4b8a13e9c4b0c08766ff7c8a3f03317da8cec3
Ashutosh Aggarwal <f20240889@pilani.bits-pilani.ac.in>
2026-09-19 13:01:43 +0000

    lab02 task3

--- labs/lab02/task3/comp2.v
+++ labs/lab02/task3/comp2.v
@@ -1,59 +1,23 @@
-// tb.v
-// Starter testbench template -- YOU complete this file.
+// comp2.v
+// 2-bit unsigned magnitude comparator.
+// Given two 2-bit values A and B, exactly one of GT, LT, EQ should be 1
+// for any input combination.
+//
+// This module has a bug that a *self-checking* testbench should catch on
+// its own -- you should not need to inspect the code below to find it.
+// Write your testbench first, let it tell you something is wrong, THEN
+// come back and fix this file.
 
-module tb;
+module comp2 (
+  input  [1:0] A,
+  input  [1:0] B,
+  output       GT,
+  output       LT,
+  output       EQ
+);
 
-  // TODO: declare the inputs and outputs
-  reg [2:0] t_sel;
-  wire [7:0]  t_dout;
-  // TODO: instantiate DUT here
-
-  lut #(.WIDTH(8), .DEPTH(8)) DUT (
-    .sel (t_sel),
-    .dout (t_dout)
-  );
-
-
-  // Waveform dump configuration (DO NOT CHANGE)
-  string vcd_file;
-  initial begin
-    if ($value$plusargs("vcd=%s", vcd_file)) begin
-      $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
-    end
-  end
-
-  initial begin
-    // TODO: apply different input combinations
-
-    t_sel = 3'b000;
-    #5;
-
-    t_sel = 3'b001;
-    #5;
-
-    t_sel = 3'b010;
-    #5;
-    
-    t_sel = 3'b011;
-    #5;
-
-    t_sel = 3'b100;
-    #5;
-
-    t_sel = 3'b101;
-    #5;
-
-    t_sel = 3'b110;
-    #5;
-
-    t_sel = 3'b111;
-    #5;
-    $finish;
-
-  end
-
-  initial
-    $monitor($time, " sel=%b --> dout=%b", t_sel, t_dout); // change as required
+  assign EQ = (A == B);
+  assign GT = (A > B);
+  assign LT = (A <  B);
 
 endmodule
